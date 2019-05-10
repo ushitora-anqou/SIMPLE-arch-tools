f0c99ee6f6045e4b536f2db59c711bc67f824ea2
Ushitora Anqou <ushitora@anqou.net>
2019-05-10 10:43:20 +0900

    Add fiblt55_macro.s

---
+++ examples/fiblt55_macro.s
@@ -0,0 +1,15 @@
+    define MAX 55
+	define A0 0
+	define A1 1
+
+    R0 = A0
+    R1 = A1
+    R4 = MAX
+loop:
+	OUT R0
+    R3 = R0
+    R0 = R1
+    R1 += R3
+    if R0 <= R4 then goto loop
+exit:
+    HLT
