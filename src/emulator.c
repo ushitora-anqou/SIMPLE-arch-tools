a4ebb9ffbe338f70b78a865611536c349373d530
Ushitora Anqou <ushitora@anqou.net>
2019-05-19 11:29:25 +0900

    [emulator] More memory dump

--- src/emulator.c
+++ src/emulator.c
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
 
     if (memdump_flag) {
         Word *mem = getMEM();
-        for (int i = 0; i < 0x1C00; i++) printf("%04X : %04X\n", i, mem[i]);
+        for (int i = 0; i < 0x2400; i++) printf("%04X : %04X\n", i, mem[i]);
     }
 
     if (!quiet_flag) printf("#insts: %d\n", ninsts);
